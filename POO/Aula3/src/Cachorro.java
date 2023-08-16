e6dae5a8f734feedada427599a01b6e575694f43
Cristopher Paiva <josuepaiva7@gmail.com>
2023-08-15 22:45:27 -0300

    tarefa semana 3

---
+++ POO/Aula3/src/Cachorro.java
@@ -0,0 +1,7 @@
+public class Cachorro {
+  private String nome;
+  private int idade;
+  private float quantVida;
+  private String corDoPelo;
+  private float forcaDaMordida;
+}
---
+++ POO/Aula3/src/Pessoa.java
@@ -0,0 +1,33 @@
+public class Pessoa {
+  private String nome;
+  private int idade;
+  private float quantVida;
+  private float habilidadeParaAtirar;
+
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public int getIdade() {
+    return idade;
+  }
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  public float getQuantVida() {
+    return quantVida;
+  }
+  public void setQuantVida(float quantVida) {
+    this.quantVida = quantVida;
+  }
+  public float getHabilidadeParaAtirar() {
+    return habilidadeParaAtirar;
+  }
+  public void setHabilidadeParaAtirar(float habilidadeParaAtirar) {
+    this.habilidadeParaAtirar = habilidadeParaAtirar;
+  }
+
+
+}
