11c884dbe6fec2e15365ee66a84eaa894b457fed
Cristopher Paiva <josuepaiva7@gmail.com>
2023-08-17 22:27:23 -0300

    evaluative activity

--- ESTRUTURADEDADOS/Aula2/src/App.java
+++ ESTRUTURADEDADOS/Aula2/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int[][] matriz = new int[10][10];
@@ -12,16 +14,20 @@ public class App {
         System.out.println("A soma dos elementos na matriz e: " + sum);
 
 
-
-        int number = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int num = scanner.nextInt();
+        boolean encontrado = false;
         for(int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz.length; j++){
-                if(matriz[i][j] == 50){
-                    number = matriz[i][j];
-                    System.out.println("O numero " + number + " esta na linha " + i + " e na coluna " + j);
+                if(matriz[i][j] == num){
+                    System.out.println("O numero " + num + " esta na linha " + i + " e na coluna " + j);
+                    encontrado = true;
                 }
             }
         }
-        
+        if(!encontrado){
+            System.out.println("O numero " + num + " nao esta na matriz");
+        }
     }
 }
