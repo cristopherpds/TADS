36e270a45645efe99ab32d7c7d60876cd8232d81
Cristopher Paiva <josuepaiva7@gmail.com>
2024-03-28 20:55:10 -0300

    add

---
+++ TESTES/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public Boolean numeroPar(int x){
+        return x % 2 == 0;
+    }
+
+    public int areaQuadrado(int lado){
+        return lado * lado;
+    }
+
+    public int soma(int a, int b){
+        return a + b;
+    }
+}
