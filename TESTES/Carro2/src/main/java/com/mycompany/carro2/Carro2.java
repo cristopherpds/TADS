479371896262ef2ce42de290947377bc17ac69af
Cristopher Paiva <josuepaiva7@gmail.com>
2024-05-09 21:23:00 -0300

    add carro2

---
+++ TESTES/Carro2/src/main/java/com/mycompany/carro2/Carro2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carro2;
+
+/**
+ *
+ * @author IFSul
+ */
+public class Carro2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
