cf8f3f5def50793f01c74c00501c08e0b41c111e
Cristopher Paiva <josuepiva7@gmail.com>
2024-06-03 12:11:36 -0300

    ajustes projeto final web1

--- WEB1/ActividadeTopico9/js/index.js
+++ WEB1/ActividadeTopico9/js/index.js
@@ -14,4 +14,16 @@ var diasFaltando = Math.ceil(diferenca / (1000 * 60 * 60 * 24));
 container.innerHTML += `Faltam <span class="font-bold pseletivo">${diasFaltando} dias</span> para o próximo Processo Seletivo do IFSul.`;
 
 
-console.log("teste")
+
+$(document).ready(function() {
+    $("#testimonial-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+        dots: true
+    });
+});
