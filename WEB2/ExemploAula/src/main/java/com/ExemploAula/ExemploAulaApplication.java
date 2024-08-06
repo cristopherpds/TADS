8706c18ce43136781006c1638c248c3c4ad165e6
Cristopher Paiva <josuepaiva7@gmail.com>
2024-08-06 19:56:24 -0300

    add pasta pra web2

---
+++ WEB2/ExemploAula/src/main/java/com/ExemploAula/ExemploAulaApplication.java
@@ -0,0 +1,13 @@
+package com.ExemploAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploAulaApplication.class, args);
+	}
+
+}
