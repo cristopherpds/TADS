45f214561fc65fc61bd57e8cd5292394957a39f1
Cristopher Paiva <josuepaiva7@gmail.com>
2024-11-13 21:14:52 -0300

    create base project

--- MOBILE/Notificacoes/app/build.gradle.kts
+++ MOBILE/Notificacoes/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
+    //id("com.android.application")
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,4 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.firebase:firebase-messaging")
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-analytics")
 }
