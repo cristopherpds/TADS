cf4400bab9fdd8f4cfde6040b30cf421e875aa50
Cristopher Paiva <josuepaiva7@gmail.com>
2024-11-15 15:12:57 -0300

    add: notificacao via FCM console

---
+++ MOBILE/IFSUL-Mobile2-Notificacao-Exemplo/app/src/main/java/br/com/rafaelamorim/exemplousonotific/MainActivity.java
@@ -0,0 +1,73 @@
+package br.com.rafaelamorim.exemplousonotific;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_POST_NOTIFICATIONS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button buttonPedirPermissao = findViewById(R.id.btnPedirPermissao);
+        Button buttonNotificacaoSimples = findViewById(R.id.btnNotificacaoSimples);
+        Button buttonRegistrarNoFCM = findViewById(R.id.btnRegistrarNoFCM);
+        Notificacao not = new Notificacao();
+
+        buttonPedirPermissao.setOnClickListener(v -> {
+            not.pedePermissao(this);
+        });
+
+        buttonNotificacaoSimples.setOnClickListener(v -> {
+            not.mostraNotificacaoLocal(this);
+        });
+
+        buttonRegistrarNoFCM.setOnClickListener(v -> {
+            Toast.makeText(this, "Essa é a sua missão 🙂", Toast.LENGTH_LONG).show();
+        });
+
+        // Solicitar permissão para notificações no Android 13+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_CODE_POST_NOTIFICATIONS);
+            }
+        }
+
+        // Inicializar o Firebase Messaging
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.w("MainActivity", "Fetching FCM registration token failed", task.getException());
+                        return;
+                    }
+
+                    // Obter o token de registro FCM
+                    String token = task.getResult();
+                    Log.d("MainActivity", "FCM Registration Token: " + token);
+                });
+    }
+}
---
+++ MOBILE/IFSUL-Mobile2-Notificacao-Exemplo/app/src/main/java/br/com/rafaelamorim/exemplousonotific/MyFirebaseMessagingService.java
@@ -0,0 +1,35 @@
+package br.com.rafaelamorim.exemplousonotific;
+
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        if (!remoteMessage.getData().isEmpty()) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Notificacao notificacao = new Notificacao();
+            notificacao.sendNotification(this, remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+    }
+}
